690d2c2c6b81cdc4bdfc1b6054d7508bda0cd3bd
Kevin Blanchard <blanchardkevinj@gmail.com>
2022-03-03 21:58:26 -0500

    Change file around to actually work

--- k8s/port_forward_to_minikube.sh
+++ k8s/port_forward_to_minikube.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-#need to get the ip and port from the minikube service command so that this is dynamic
-#also need to remove this when you are stopping minikube
-if [ $# -ne 2]; then
-    echo 'Wrong number of params'
-    exit 1
+#Need to add a removal shell script as well
+
+if [ "$#" != "1" ]; then
+	echo 'You need to pass in 1 paramte that is the service name.'
+	exit 1
 fi
-port=$1
-ip=$2
-echo $port and $ip
+service_name=$1
+
+response=`minikube service $service_name --url | sed "s/http:\/\///"`
+ip=`echo $response | cut -d ':' -f1`
+port=`echo $response | cut -d ':' -f2`
+echo $ip and $port
+
+#port=$1
+#ip=$2
+#echo $port and $ip
 
 #sudo iptables -A PREROUTING -t nat -i wlp0s20f3 -p tcp --dport $port -j DNAT --to $ip:$port
 #sudo iptables -A FORWARD -p tcp -d $ip --dport $port -j ACCEPT
