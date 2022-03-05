129baa800080e08afe1697c4f6b25e7164f799e7
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-05 10:33:21 -0500

    Add in 443 support to start with ssl

--- nginx/get_ssl_keys.sh
+++ nginx/get_ssl_keys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #This is used to request a new certificate from letsencrypt.  The first command allows us to start out webserver properly with the second command, only run if there is no certs.
-if [ "$(ls -A cert_folder)" ]; then
+if [ "$(ls -A $CERT_FOLDER)" ]; then
     cp /etc/nginx/conf.d/default.conf.certbot /etc/nginx/conf.d/default.conf
     openssl req -batch -newkey rsa:1024 -new -nodes -x509 -days 2 -keyout $KEYS_FOLDER/privkey.pem -out $KEYS_FOLDER/fullchain.pem
     certbot certonly --webroot -w /var/www/challenge -d www.wedding.supergoon.com -d wedding.supergoon.com -m blanchardkevinj@gmail.com --agree-tos
