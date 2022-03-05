31bccb35193ca08e878974d7312c09e5921fbdf6
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-05 09:48:57 -0500

    Modify

--- ansible/mount_minikube_volume.sh
+++ ansible/mount_minikube_volume.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 # minikube mount <source directory>:<target directory>
 minikube start --driver=docker --mount /kube_host
-minikube mount /etc/letsencrypt:/etc/letsencrypt
-minikube mount /var/www/challenge/.well-known/acme-challenge enf3rno/sg_certbot:/var/www/challenge/.well-known/acme-challenge enf3rno/sg_certbot
