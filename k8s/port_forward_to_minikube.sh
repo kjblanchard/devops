2c77187fd93fe352e1c957e3e7ec9c20b2170e99
Kevin Blanchard <blanchardkevinj@gmail.com>
2022-03-05 15:43:29 -0500

    Fix syntax

--- k8s/port_forward_to_minikube.sh
+++ k8s/port_forward_to_minikube.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #Need to add a removal shell script as well
+get_url_add_to_iptables(){
+	without_http=`echo $1 | sed "s/http:\/\///"`
+	ip=`echo $without_http | cut -d ':' -f1`
+	port=`echo $without_http | cut -d ':' -f2`
+	sudo iptables -A PREROUTING -t nat -i wlp0s20f3 -p tcp --dport $port -j DNAT --to $ip:$port
+	sudo iptables -A FORWARD -p tcp -d $ip --dport $port -j ACCEPT
+}
 
 if [ "$#" == "1" ]; then
 	service_name=$1
@@ -12,12 +19,5 @@ while (( "$#" )); do        # While there are arguments still to be shifted
 done
 fi
 
-get_url_add_to_iptables(){
-	without_http=`echo $1 | set "s/http:\/\///`
-	ip=`echo $without_http | cut -d ':' -f1`
-	port=`echo $without_http | cut -d ':' -f2`
-	sudo iptables -A PREROUTING -t nat -i wlp0s20f3 -p tcp --dport $port -j DNAT --to $ip:$port
-	sudo iptables -A FORWARD -p tcp -d $ip --dport $port -j ACCEPT
-}
 
 
