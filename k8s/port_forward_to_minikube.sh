71c26917923e768a474e63a62a9836a7c489ee37
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-05 15:32:56 -0500

    Update port forward script to handle 2

--- k8s/port_forward_to_minikube.sh
+++ k8s/port_forward_to_minikube.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 #Need to add a removal shell script as well
 
-if [ "$#" != "1" ]; then
-	echo 'You need to pass in 1 paramte that is the service name.'
-	exit 1
+if [ "$#" == "1" ]; then
+	service_name=$1
+	response=`minikube service $service_name --url`
+	get_url_add_to_iptables $response
+else
+	while (( "$#" )); do        # While there are arguments still to be shifted
+	get_url_add_to_iptables $1
+
+	done
+	shift
+done
 fi
-service_name=$1
 
-response=`minikube service $service_name --url | sed "s/http:\/\///"`
-ip=`echo $response | cut -d ':' -f1`
-port=`echo $response | cut -d ':' -f2`
-echo $ip and $port
+get_url_add_to_iptables(){
+	without_http=`echo $1 | set "s/http:\/\///`
+	ip=`echo $without_http | cut -d ':' -f1`
+	port=`echo $without_http | cut -d ':' -f2`
+	sudo iptables -A PREROUTING -t nat -i wlp0s20f3 -p tcp --dport $port -j DNAT --to $ip:$port
+	sudo iptables -A FORWARD -p tcp -d $ip --dport $port -j ACCEPT
+}
 
-#sudo iptables -A PREROUTING -t nat -i wlp0s20f3 -p tcp --dport $port -j DNAT --to $ip:$port
-#sudo iptables -A FORWARD -p tcp -d $ip --dport $port -j ACCEPT
+
