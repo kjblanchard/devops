06db07e219d47ba524cc66adc8847564d178cd99
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 14:37:07 -0400

    test

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -11,6 +11,14 @@ void get_changed_files(){
     def script_content = libraryResource('get_changed_files.sh')
     writeFile file: './get_changed_files.sh', text: script_content
     sh(script: 'chmod +x get_changed_files.sh')
+    def changed_files_1 = sh(
+        script: """
+            ls
+            git diff --name-only HEAD HEAD~1
+        """
+        returnStdout: true
+    )
+    echo changed_files_1
     def changed_files = sh(
         script: './get_changed_files.sh',
         returnStdout: true
