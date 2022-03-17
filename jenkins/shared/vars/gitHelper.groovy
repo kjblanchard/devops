ce03864fe1661d424456f3e5ca0cc705811f9960
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 13:32:13 -0400

    testing pipeline

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -2,9 +2,11 @@ import groovy.transform.Field
 @Field changed_files
 
 def call(){
+    echo 'Just got into the githelper call'
     get_changed_files()
 }
 void get_changed_files(){
+    echo 'Just got to the changed files section'
     def script_content = libraryResource 'bash/get_changed_files.sh'
     writeFile file: './get_changed_files.sh', text: script_content
     def changed_files = sh(
