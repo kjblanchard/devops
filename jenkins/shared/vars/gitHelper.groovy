c9732497659d9cd7ba3c5e5e0c6dce13a38a535c
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 13:30:20 -0400

    Syntax

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -8,7 +8,7 @@ void get_changed_files(){
     def script_content = libraryResource 'bash/get_changed_files.sh'
     writeFile file: './get_changed_files.sh', text: script_content
     def changed_files = sh(
-        script: './get_changed_files.sh'
+        script: './get_changed_files.sh',
         returnStdout: True
     ).split()
     echo changed_files
