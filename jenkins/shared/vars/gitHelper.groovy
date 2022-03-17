af6751f6362d5987de65e1e38a7f0e434f5af0f0
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 13:38:55 -0400

    Update relative path maybe

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -7,7 +7,7 @@ def call(){
 }
 void get_changed_files(){
     echo 'Just got to the changed files section'
-    def script_content = libraryResource 'bash/get_changed_files.sh'
+    def script_content = libraryResource './bash/get_changed_files.sh'
     writeFile file: './get_changed_files.sh', text: script_content
     def changed_files = sh(
         script: './get_changed_files.sh',
