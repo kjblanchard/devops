8c5a27b5cdb68fb3661638b66789675fd4173164
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 19:36:01 -0400

    Use fixed yaml in second file

--- jenkins/shared/vars/pipelineTemplate.groovy
+++ jenkins/shared/vars/pipelineTemplate.groovy
@@ -1,31 +1,31 @@
 import groovy.transform.Field
 @Field agent_yaml
 agent_yaml = """
-    apiVersion: v1
-    kind: Pod
-    metadata:
-    labels:
-    component: ci
-    spec:
-    serviceAccountName: jenkins
-    containers:
-    - name: docker
-        image: docker:latest
-        command:
-        - cat
-        tty: true
-        volumeMounts:
-        - mountPath: /var/run/docker.sock
-        name: docker-sock
-    - name: git
-        image: alpine/git:latest
-        command:
-        - cat
-        tty: true
-    volumes:
-        - name: docker-sock
-        hostPath:
-            path: /var/run/docker.sock
+apiVersion: v1
+kind: Pod
+metadata:
+labels:
+  component: ci
+spec:
+  serviceAccountName: jenkins
+  containers:
+  - name: docker
+    image: docker:latest
+    command:
+    - cat
+    tty: true
+    volumeMounts:
+    - mountPath: /var/run/docker.sock
+      name: docker-sock
+  - name: git
+    image: alpine/git:latest
+    command:
+    - cat
+    tty: true
+  volumes:
+    - name: docker-sock
+      hostPath:
+        path: /var/run/docker.sock
     """
 
 def call(Map config_map = [:], Map stages_map = [:], String agent_yaml_string = agent_yaml)
