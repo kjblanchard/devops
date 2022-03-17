382b5d14b68a86e599dc12680052bd16c9b377d6
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 18:43:16 -0400

    Add in some tests

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -11,9 +11,13 @@ void get_changed_files(Map config_map){
     echo 'Just got to the changed files section'
     changed_files = sh(
         script: """
+        echo hi
+        ls
             git diff-tree --no-commit-id --name-only -r HEAD
         """,
         returnStdout: true
     ).trim()
+    echo 'just finished this'
     config_map.changed_files = changed_files
+    echo 'now just finished this'
 }
