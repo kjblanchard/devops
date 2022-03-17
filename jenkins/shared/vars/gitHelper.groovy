02e77adb863201e9110dc79c8302d90e62a12617
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 19:00:57 -0400

    Move out of func

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -1,22 +1,34 @@
 import groovy.transform.Field
-// @Field changed_files
+@Field changed_files
 
 
 def call(Map config_map){
     echo 'Just got into the githelper call'
-    get_changed_files(config_map)
+    // get_changed_files(config_map)
+        echo 'Just got to the changed files section'
+    changed_files = sh(
+        script: """
+        echo hi
+        ls
+            git diff-tree --no-commit-id --name-only -r HEAD
+        """,
+        returnStdout: true
+    ).trim()
+    echo 'just finished this'
+    config_map.changed_files = changed_files
+    echo 'now just finished this'
 }
 
 void get_changed_files(Map config_map){
     echo 'Just got to the changed files section'
-    // def changed_files = sh(
-    //     script: """
-    //     echo hi
-    //     ls
-    //         git diff-tree --no-commit-id --name-only -r HEAD
-    //     """,
-    //     returnStdout: true
-    // ).trim()
+    def changed_files = sh(
+        script: """
+        echo hi
+        ls
+            git diff-tree --no-commit-id --name-only -r HEAD
+        """,
+        returnStdout: true
+    ).trim()
     echo 'just finished this'
     config_map.changed_files = changed_files
     echo 'now just finished this'
