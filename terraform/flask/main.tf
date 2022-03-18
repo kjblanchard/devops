b56f166af85882d893d51a34e6557ba0e58d8b2f
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-18 19:32:05 -0400

    Update

--- terraform/flask/main.tf
+++ terraform/flask/main.tf
@@ -2,6 +2,6 @@ module "flask_deployment" {
   source          = "../modules/deployment"
   image_name      = "enf3rno/sg_flask"
   image_tag       = "dev"
-  deployment_name = "flask"
+  deployment_name = "flask_wedding"
   container_port  = 80
 }
