c8674911d09a431ec6ed6b4362e14ffb47d7ab7f
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 21:29:07 -0400

    test

--- jenkins/shared/vars/gitHelper.groovy
+++ jenkins/shared/vars/gitHelper.groovy
@@ -5,23 +5,18 @@ def call(Map config_map, ArrayList stage_list){
             container('git')
             {
                 echo 'Just got into the githelper call'
-                def changed_files = sh(
-                    script: """
-                    ls
-                    git status
-                    git log --oneline
-                        git diff --name-only HEAD HEAD~1
-                    """,
-                    returnStdout: true
-                ).trim()
-                config_map.changed_files = changed_files
-                // get_changed_files(config_map)
+                get_changed_files(config_map)
+            }
+            container('docker')
+            {
+                sh 'ls'
             }
         }
     })
 }
 
 void get_changed_files(Map config_map){
+    sh 'ls'
     def changed_files = sh(
         script: """
         ls
