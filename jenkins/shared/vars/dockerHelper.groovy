13b594b96ee82fb893ba9d32f2909d21a5a76161
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-17 22:30:37 -0400

    test a push

--- jenkins/shared/vars/dockerHelper.groovy
+++ jenkins/shared/vars/dockerHelper.groovy
@@ -1,20 +1,17 @@
 def call(Map config = [:], ArrayList stage_list )
 {
-    stage_list.add(
-    {
+    stage_list.add({
         stage('Determine Builds') {
-                def changed_files = config.changed_git_files
-                if (changed_files =~ /docker/){
-                    docker_image_builder(stage_list, 'sg_flask', 'flask')
-                    docker_image_pusher(stage_list, 'sg_flask')
-                }
-                if(changed_files =~ /git/){
-                    echo 'GIT CHANGED'
-                }
+            def changed_files = config.changed_git_files
+            if (changed_files =~ /docker/){
+                docker_image_builder(stage_list, 'sg_flask', 'flask')
+                docker_image_pusher(stage_list, 'sg_flask')
+            }
+            if(changed_files =~ /git/){
+                echo 'GIT CHANGED'
+            }
         }
-    }
-    )
-        // stage_map.docker = stage
+    })
 }
 
 def docker_image_builder(ArrayList stage_list, String build_name, String folder_name) {
@@ -33,10 +30,12 @@ def docker_image_pusher(ArrayList stage_list, String build_name) {
     stage_list.add({
         stage("Push ${build_name} image")
             container('docker') {
-
-                sh """
-                    docker image push ${build_name}:$BUILD_NUMBER
-                """
+                withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'password', usernameVariable: 'username')]) {
+                    sh """
+                        docker login -u ${username} -p ${password}
+                        docker image push ${username}/${build_name}:$BUILD_NUMBER
+                    """
+                }
             }
     })
 }
