aa8ca6f7b440f4942c679c459a0724d1f8be0397
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-18 23:09:49 -0400

    tf syntax

--- terraform/nginx/main.tf
+++ terraform/nginx/main.tf
@@ -1,8 +1,8 @@
 module "flask_deployment" {
   source          = "../modules/deployment"
-  image_name      = "enf3rno/quotegenerator"
-  image_tag       = "dev"
-  deployment_name = "flask"
+  image_name      = "enf3rno/sg_nginx"
+  image_tag       = "latest"
+  deployment_name = "nginx-wedding"
   container_port  = 80
   ports = [
     {
