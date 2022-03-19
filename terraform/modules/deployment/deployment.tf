c8dbe37c908adf2625bcdd5b63b0250d3f6e4c46
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-03-18 21:59:30 -0400

    tf syntax

--- terraform/modules/deployment/deployment.tf
+++ terraform/modules/deployment/deployment.tf
@@ -22,9 +22,9 @@ resource "kubernetes_deployment" "k8s_deployment" {
         container {
           image = "${var.image_name}:${var.image_tag}"
           name  = "${var.deployment_name}"
-          port {
-            container_port = "${var.container_port}"
-          }
+        #   port {
+        #     container_port = "${var.container_port}"
+        #   }
             dynamic "volume_mount"{
                 for_each = var.volume_mounts
                 content {
