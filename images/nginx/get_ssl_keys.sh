cb020ceaf11196be0c3ef8266fb08abadf5859db
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-03-10 19:52:35 -0500

    update ssl keys

--- images/nginx/get_ssl_keys.sh
+++ images/nginx/get_ssl_keys.sh
@@ -10,12 +10,13 @@ generate_cert(){
 
 configure_prod_server(){
     cp /etc/nginx/conf.d/default.conf.webserver /etc/nginx/conf.d/default.conf
+    nginx
 }
 if [ -f "$CERT_FOLDER"/fullchain.pem ]; then
-    echo 'Cert is available, not generating cert'
+    echo 'Cert is available, not generating cert' >> /proc/1/fd/1
     configure_prod_server
  else
-    echo 'Generating cert, then starting'
+    echo 'Generating cert, then starting' >> /proc/1/fd/1
     generate_cert
     configure_prod_server
 fi
