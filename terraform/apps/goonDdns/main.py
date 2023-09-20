62af87cbe8ed2defca42bfa0cbed7b8814283cd3
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-09-20 19:50:38 -0400

    Try using different env variable

--- terraform/apps/goonDdns/main.py
+++ terraform/apps/goonDdns/main.py
@@ -4,8 +4,8 @@ import json
 env_vars = {
     "DNS_ZONES": os.getenv("DNS_ZONES"),
     "ZONE_ID": os.getenv("ZONE_ID"),
-    "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
-    "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY")
+    "AWS_ACCESS_KEY_ID": os.getenv("DDNS_AWS_ACCESS_KEY_ID"),
+    "AWS_SECRET_ACCESS_KEY": os.getenv("DDNS_AWS_SECRET_ACCESS_KEY")
 }
 
 print(json.dumps(env_vars))
