256260d51dc525bd8b4a805baf7554e5eec3c622
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-03-21 18:26:53 -0400

    use new nginx with proper wedding

--- terraform/apps/nginx/main.tf
+++ terraform/apps/nginx/main.tf
@@ -2,7 +2,7 @@ module "flask_deployment" {
   source          = "../../modules/deployment"
   image_name      = "enf3rno/sg_nginx"
   #image_tag       = "2"
-  image_tag       = "27"
+  image_tag       = "28"
   deployment_name = "nginx-wedding"
   ports = [
     {
